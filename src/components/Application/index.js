489fb218ace3133c99476bc460d95646edf13f70
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-10-19 16:53:27 +0100

    Setup the project

---
+++ src/components/Application/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Application extends Component {
+  render() {
+    return (
+      <div className="Application">
+        <header className="Application-header">
+          <img src="https://is3-ssl.mzstatic.com/image/thumb/Purple111/v4/f3/1a/37/f31a3705-2930-81ea-7267-e4ad36118ccb/source/256x256bb.jpg" alt="logo" />
+          <p>
+            The little book of abuse.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Application;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Application from './components/Application';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Application />, document.getElementById('root'));
 
