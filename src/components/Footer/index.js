ed235f8f3b5e3a674a3f470e73f7c70ad752d0ca
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-10-22 20:38:00 +0100

    Refactor components, change project structure

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Header">
+        I am header
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        I am header
+      </div>
+    );
+  }
+}
+
+export default Header;
