008de0253781f7ae43e8af9390c08b41a640066a
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-10-29 21:29:47 +0000

    Redirecting on logout

--- src/pages/MyAccount/index.js
+++ src/pages/MyAccount/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
 import Button from '../../components/Button';
 import Header from '../../components/Header';
@@ -7,13 +8,26 @@ import DisplayAllReminders from '../../journey/DisplayAllReminders';
 import styles from './styles.css';
 
 class MyAccount extends Component {
+    constructor() {
+        super();
+
+    this.handleLogOut = this.handleLogOut.bind(this);
+    }
+
+    handleLogOut(e) {
+        e.preventDefault();
+        this.props.history.push('/goodbye');
+    }
+
     render() {
-        const labelLogOut = 'Click to log out';
 
         return (
             <div className={styles.MyAccount}>
+            
             <Header/>
-                <Button id='myAccountButton' label={labelLogOut} onClick={this.handleClick}/>
+
+                <Button id='logOutButton' label='Log Out' onClick={this.handleLogOut}/>
+                
                 <div className={styles.YesNoQuestion}>
                     <h2>
                         Do you want to pin a message Dear Mr/Mrs name?
@@ -38,4 +52,4 @@ class MyAccount extends Component {
     }
 }
 
-export default MyAccount;
+export default withRouter(MyAccount);
